4720307e270f57736efd78326de2fb95b289c156
sabababluani <158265615+sabababluani@users.noreply.github.com>
2025-07-10 16:37:10 +0400

    feat: removed ts folder

--- src/ts/dto/update-t.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateTDto } from './create-t.dto';
-
-export class UpdateTDto extends PartialType(CreateTDto) {}
--- src/ts/ts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TsService } from './ts.service';
-import { TsController } from './ts.controller';
-
-@Module({
-  controllers: [TsController],
-  providers: [TsService],
-})
-export class TsModule {}
